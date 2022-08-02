3ff249309bb04384c267944b53f3ed1ded411c44
Ccucumber12 <chris20021112@gmail.com>
2022-08-02 22:45:38 +0800

    Add SimpleTask

---
+++ SimpleTask/gen/gen.cpp
@@ -0,0 +1,10 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main(int argc, char *argv[]) {
+  registerGen(argc, argv, 1) ;
+  int a = atoi(argv[1]) ;
+  int b = atoi(argv[2]) ;
+  printf("%d %d\n", a, b) ;
+}
---
+++ SimpleTask/validator/val.cpp
@@ -0,0 +1,12 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+const int R = 2000000000 ;
+
+int main() {
+	registerValidation();
+	int a = inf.readInt(-R, R) ; inf.readSpace() ;
+	int b = inf.readInt(-R, R) ; inf.readEoln() ;
+	inf.readEof() ;
+}
